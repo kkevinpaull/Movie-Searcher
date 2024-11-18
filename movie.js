a4cd853d169a6a4fd6a4e61f6a9733198ee238fb
Kevin <castanetokevin@gmail.com>
2024-11-18 10:03:45 -0800

    Still in progress...

--- movie.js
+++ movie.js
@@ -1,7 +1,32 @@
 async function main() {
-    const movie = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=c1daa9fc&s=fast");
-    const movieData = await movie.json();
-    console.log(movieData)
+    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=c1daa9fc&s=galaxy");
+    const moviesData = await movies.json();
+    // console.log(movieData)
+    if (moviesData.Response === "True") {
+        const movieListEl = document.querySelector(".movies");
+        movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
+    } else {
+        console.error("Error fetching movies:", moviesData.Error);
+    }
 }
 
 main()
+
+function movieHTML(movie) {
+    return  `<div class="movies">
+    <div class="movie">
+      <figure class="movie__img--wrapper">
+        <img
+          class="movie__img"
+          src="${movie.Poster}"
+          alt=""
+        />
+      </figure>
+      <div class="movie__title">${movie.Title}</div>
+      <div class="movie__ratings">Ratings</div>
+      <div class="movie__year">Year released: ${movie.Year}</div>
+      <div class="movie__Cast">Cast:</div>
+      <div class="movie__director">Director: </div>
+    </div>
+  </div>`
+}
