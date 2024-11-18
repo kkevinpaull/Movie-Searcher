cfd084fda31d9326d796701cf603fa5544c9362c
Kevin <castanetokevin@gmail.com>
2024-11-18 08:21:38 -0800

    Landing Page

--- movie.js
+++ movie.js
@@ -1,5 +1,7 @@
 async function main() {
-    const movie = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=c1daa9fc&s=${title}`);
+    const movie = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=c1daa9fc&s=fast");
     const movieData = await movie.json();
     console.log(movieData)
 }
+
+main()
